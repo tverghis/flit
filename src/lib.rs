f73b98064bd4dae03a7ccd74d5f8fa4548a9bcb4
Tarun Verghis <tarun.verghis@gmail.com>
2019-04-03 21:50:56 +0530

    Add crate-level documentation, revise other docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! `BloomFilter` is a probabilistic data structure that can quickly and definitively conclude that it does
+//! *not* contain an item. On the other hand, it can only conclude that it *probably* contains an
+//! item, i.e., the data structure has an inherent false-positive rate greater than 0%.
+//!
+//! Items can be added to the Bloom filter, but cannot be removed - this would introduce false
+//! negative cases. If this is required, an alternative might be to use a Counting Bloom filter
+//! (not (yet) implemented in this crate).
+//!
+//! This implementation is backed by a Rust implementation of the [xxHash hashing
+//! algorithm](https://github.com/Cyan4973/xxHash), [twox-hash](https://crates.io/crates/twox-hash).
+//!
+//! # References
+//! - [Less Hashing, Same Performance: Building a Better Bloom
+//! Filter](https://www.eecs.harvard.edu/~michaelm/postscripts/rsa2008.pdf)
+//! - [Wikipedia article](https://en.wikipedia.org/wiki/Bloom_filter)
+//! - [Bloom Filters by Example](https://llimllib.github.io/bloomfilter-tutorial/)
+//! - [Bloom Filter Calculator](https://hur.st/bloomfilter/)
 use bitvec::{bitvec, BitVec};
 use std::f64::consts::{E, LN_2};
 use std::hash::{Hash, Hasher};
@@ -6,20 +23,22 @@ use twox_hash::XxHash;
 
 const LN2_SQUARED: f64 = LN_2 * LN_2;
 
-/// `BloomFilter` is a probabilistic data structure that can definitively conclude that it does
-/// *not* contain an item. On the other hand, it can only conclude that it *probably* contains an
-/// item.
+/// Represents a Bloom filter.
 ///
-/// Items can be added to the Bloom filter, but cannot be removed - this would introduce false
-/// negative cases. Once added, the structure can be quickly queried for the existence of the item.
+/// When constructing the filter using `new`, you need to specify the desired acceptable
+/// false-positive rate, and the number of items you intend to store in the filter. Neither can be
+/// adjusted after creation - create a new filter instead.
 ///
-/// Some good references include:
-/// - [Less Hashing, Same Performance: Building a Better Bloom
-/// Filter](https://www.eecs.harvard.edu/~michaelm/postscripts/rsa2008.pdf)
-/// - [Wikipedia article](https://en.wikipedia.org/wiki/Bloom_filter)
-/// - [Bloom Filters by Example](https://llimllib.github.io/bloomfilter-tutorial/)
-/// - [Bloom Filter Calculator](https://hur.st/bloomfilter/)
-#[derive(Debug)]
+/// # Example
+/// ```rust
+/// use flit::BloomFilter;
+///
+/// let mut filter = BloomFilter::new(0.01, 10000);
+/// filter.add(&"Hello, world!");
+///
+/// assert_eq!(filter.might_contain(&"Hello, world!"), true); // probably true
+/// assert_eq!(filter.might_contain(&"Dogs are cool!"), false); // definitely false!
+/// ```
 pub struct BloomFilter<T> {
     n: u64,
     m: u64,
@@ -134,7 +153,7 @@ mod tests {
 
     #[test]
     fn test_add() {
-        let mut filter = BloomFilter::<&str>::new(0.03_f64, 10);
+        let mut filter = BloomFilter::new(0.03_f64, 10);
 
         filter.add(&"Hello, world!");
 
